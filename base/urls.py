8abe84abf1e8182a9d537919a9e663940d831fa8
AlPilipenko <almondiris@gmail.com>
2021-04-17 15:28:38 +0100

    setting up

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,7 @@
 # how we create url patterns here
 from django.urls import path  # importing path function
 from . import views  # when we add new views they will be added here
-from .views import TaskList
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 
 """
@@ -11,6 +11,17 @@ as_view method
 """
 
 urlpatterns = [
-    path('/test', views.test, name='test'),
+    path('test/', views.test, name='test'),  # dynamic way to access URLs
     path('', TaskList.as_view(), name='tasks'),
+
+    # "this view by default looks for PRIMARY_KEY value (pk-value)"
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+
+    #
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+
+    # Here we also need PRIMARY_KEY so that Django knows what task we updating
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
+
 ]
--- base/views.py
+++ base/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+"importing different views:"
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Task
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+"To redirect user to a certain part af our app"
+from django.urls import reverse_lazy
+
+from .models import Task
 
 def test(request):
     return HttpResponse("Hello World")
@@ -23,12 +29,80 @@ class TaskList(ListView):
     By default Django calls the query - object_list
     But we can change it:
     """
-
     context_object_name = 'tasks'
 
 
-class Fr(DetailView):
+class TaskDetail(DetailView):
     """
     Returns information about simple items.
     So when we click on the task we want more information about this item.
     """
+    model = Task
+    context_object_name = 'task'
+
+    """
+    By default this view is looking for a model name + prefix _detail, which
+    is task_detail.html
+    To costumize template name for django to look for
+    """
+    template_name = 'base/task.html'
+
+
+class TaskCreate(CreateView):
+    """
+    It has more complex logic than previous view, because we are sending post
+    requests and we have to create an item.
+    """
+    model = Task
+    "Not changing template name here because it will mess up TaskUpdate"
+    # template_name = 'base/task-create.html'
+
+    """
+    By default the CreateView already giving us a model (Task) Form to work with.
+    Model form - a class representation of a form based on a model. That means
+    it going to take "Task" model and its going to create all the fields by
+    default for us. So we only need to specify the fields that we want.
+    """
+
+    # field = ['title', 'desctiption']
+    fields = '__all__'  # to list out all of the items of the field
+
+    """
+    After form is submitted we want to redirect user to a different page.
+    So we need to set up success_url:
+    """
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    """
+    UpdateView takes an item, prefills the form and one submited modifies data.
+    """
+    model = Task
+
+    """
+    By default this view is also looking for a model name + prefix _form,
+    since we already have this template - task_form.html we don't need to
+    create another one.
+    """
+    # template_name = 'base/task-update.html'
+
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskDelete(DeleteView):
+    """
+    It does two thigs: 1)Renders a page that says "are you sure"?
+                       2)And if wish to process it will send a post request and
+                         it will delete the item.
+    """
+
+    model = Task
+
+    "By default context_object_name is 'object', so we'll change it to task"
+    context_object_name = 'task'
+
+    "By default DeleteView is looking for a model(Task) + _confirm_delete.html"
+
+    success_url = reverse_lazy('tasks')
