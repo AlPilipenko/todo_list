3122668431c53b14c02ebb8b49a0d3a7dcdd5a5b
AlPilipenko <almondiris@gmail.com>
2021-04-23 10:20:41 +0100

    set pass to env var

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -19,8 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p3nf6^khio9)mc3g$m53cx%#_f68azyp=czga@yyk_ai6!+kz4'
+import os
+
+SECRET_KEY = os.environ.get('TODO_LIST_PASS')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
